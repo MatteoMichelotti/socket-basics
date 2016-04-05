ba5782fccd6028084f47fe1ec2ba6433bc500273
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-04-05 18:07:55 -0500

    Add custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on("connect", function() {
 	console.log("Connected to Socket.io server!");
 });
+
+socket.on("message", function (data){
+	console.log("New message: " + data.text);
+});
--- server.js
+++ server.js
@@ -3,13 +3,23 @@ var express = require("express");
 var app = express();
 
 var http = require("http").Server(app);
-
 var io = require("socket.io")(http);
 
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", function (){
-	console.log("User connected via socket.io!")
+io.on("connection", function (socket){
+	console.log("User connected via socket.io!");
+
+	socket.on("message", function (message){
+		console.log("New message received: " + message.text);
+
+		// io.emit("message", message);
+		socket.broadcast.emit("message", message);
+	});
+
+	socket.emit("message", {
+		text: "Welcome to the chat application!"
+	});
 });
 
 
