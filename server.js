0659c932509a9d9134f8c524798946e67e03294f
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-04-07 15:41:02 -0500

    Add @currentUsers command

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var PORT = process.env.PORT || 3000;
+var COMMANDS = ["@currentUsers"];
+
 var express = require("express");
 var app = express();
 
@@ -10,6 +12,20 @@ var io = require("socket.io")(http);
 app.use(express.static(__dirname + "/public"));
 
 var clientInfo = {};
+
+function getCurrentUsers (socket) {
+	var users = [];
+	var info = clientInfo[socket.id];
+
+	if (info) {
+		Object.keys(clientInfo).forEach(function (userID) {
+			if (clientInfo[userID].room === info.room) {
+				users.push(clientInfo[userID].name);
+			}
+		});
+		return users.join(", ");
+	}
+}
 
 io.on("connection", function (socket){
 	console.log("User connected via socket.io!");
@@ -27,11 +43,25 @@ io.on("connection", function (socket){
 	socket.on("message", function (message){
 		console.log("New message received: " + message.text);
 
-		io.to(clientInfo[socket.id].room).emit("message", {
-			text: message.text,
-			name: message.name,
-			timestamp: +moment()
-		});
+		var info = clientInfo[socket.id];
+
+		switch (COMMANDS.indexOf(message.text)){
+			case 0:
+				//@currentUsers
+				socket.emit("message", {
+					text: "Current users:" + getCurrentUsers(socket),
+					name: "System",
+					timestamp: +moment()
+				});
+				break;
+			case -1: 
+				//Normal message
+				io.to(info.room).emit("message", {
+					text: message.text,
+					name: message.name,
+					timestamp: +moment()
+				});
+		}				
 	});
 
 	socket.on("disconnect", function (){
