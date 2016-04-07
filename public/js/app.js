3fcf3594e7ec4fafadee98402a39696f1e975cce
Matteo Michelotti <matteo.michelotti@outlook.com>
2016-04-06 21:50:36 -0500

    Add chat div to display messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,20 @@
 var socket = io();
 
+var $form = $("#message-form");
+var $messageInput = $("input[name=message]");
+var $chat = $("#chat");
+
 socket.on("connect", function() {
 	console.log("Connected to Socket.io server!");
 });
 
 socket.on("message", function (data){
 	console.log("New message: " + data.text);
+
+	$chat.append("> " + data.text + "<br>");
 });
 
 // Handles submitting of new message
-var $form = $("#message-form");
-var $messageInput = $("input[name=message]");
 
 $form.on("submit", function (event){
 	event.preventDefault();
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ io.on("connection", function (socket){
 	socket.on("message", function (message){
 		console.log("New message received: " + message.text);
 
-		// io.emit("message", message);
-		socket.broadcast.emit("message", message);
+		io.emit("message", message);
+		// socket.broadcast.emit("message", message);
 	});
 
 	socket.emit("message", {
